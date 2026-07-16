75f84d3dff50daa52f24e36c8012e38451486817
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:49:43 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "network_manager_routing_configurations_id" {
   description = "Map of id values across all network_manager_routing_configurations, keyed the same as var.network_manager_routing_configurations"
-  value       = { for k, v in azurerm_network_manager_routing_configuration.network_manager_routing_configurations : k => v.id }
+  value       = { for k, v in azurerm_network_manager_routing_configuration.network_manager_routing_configurations : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "network_manager_routing_configurations_description" {
   description = "Map of description values across all network_manager_routing_configurations, keyed the same as var.network_manager_routing_configurations"
-  value       = { for k, v in azurerm_network_manager_routing_configuration.network_manager_routing_configurations : k => v.description }
+  value       = { for k, v in azurerm_network_manager_routing_configuration.network_manager_routing_configurations : k => v.description if v.description != null && length(v.description) > 0 }
 }
 output "network_manager_routing_configurations_name" {
   description = "Map of name values across all network_manager_routing_configurations, keyed the same as var.network_manager_routing_configurations"
-  value       = { for k, v in azurerm_network_manager_routing_configuration.network_manager_routing_configurations : k => v.name }
+  value       = { for k, v in azurerm_network_manager_routing_configuration.network_manager_routing_configurations : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "network_manager_routing_configurations_network_manager_id" {
   description = "Map of network_manager_id values across all network_manager_routing_configurations, keyed the same as var.network_manager_routing_configurations"
-  value       = { for k, v in azurerm_network_manager_routing_configuration.network_manager_routing_configurations : k => v.network_manager_id }
+  value       = { for k, v in azurerm_network_manager_routing_configuration.network_manager_routing_configurations : k => v.network_manager_id if v.network_manager_id != null && length(v.network_manager_id) > 0 }
 }
 output "network_manager_routing_configurations_route_table_usage_mode" {
   description = "Map of route_table_usage_mode values across all network_manager_routing_configurations, keyed the same as var.network_manager_routing_configurations"
-  value       = { for k, v in azurerm_network_manager_routing_configuration.network_manager_routing_configurations : k => v.route_table_usage_mode }
+  value       = { for k, v in azurerm_network_manager_routing_configuration.network_manager_routing_configurations : k => v.route_table_usage_mode if v.route_table_usage_mode != null && length(v.route_table_usage_mode) > 0 }
 }
 
